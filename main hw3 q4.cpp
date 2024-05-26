085f02eac0a26d10a5c75737028d6d5d39a62d3f
cindyh2 <143455603+cindyh2@users.noreply.github.com>
2024-05-25 20:16:30 -0400

    Update main hw3 q4.cpp

--- main hw3 q4.cpp
+++ main hw3 q4.cpp
@@ -16,17 +16,19 @@ int main() {
     std::unique_ptr<int[]> smartArray(new int[10]);
     
     // Initialize the array with values 1 through 10
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 10; i++) {
         smartArray[i] = i + 1; //intializing the array
     }
     
     // finding the sum of the values in the array
     int sums = 0;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 10; i++) {
         sums += smartArray [i];
     }
     
     cout << "Sum with the smart pointer: " << sums << endl;
+    //smartArray will be automatically deleted
+
     
     // using std::unique_ptr ensures that the allocated memory is deallocated as soon as the pointer is not utilized (out of scpre) anymore
     // this is especially useful when a dynamic array is not added in a coding session
